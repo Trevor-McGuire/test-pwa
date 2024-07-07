cf0aa9e4f05130eeecaf6ff8498942b27ecf9dfe
Trevor-McGuire <121839229+Trevor-McGuire@users.noreply.github.com>
2024-07-07 18:33:26 -0500

    changed service worker url

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Check if service worker is supported
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(registration => {
+    navigator.serviceWorker.register('/test-pwa/sw.js').then(registration => {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
       // Request notification permission on load
       Notification.requestPermission().then(permission => {
